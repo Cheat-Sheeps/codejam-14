30d17ae72255d1e3d0cf5d6ff6c64ff7a03d0b18
Theodore L'Heureux <theodorelheureux@gmail.com>
2024-11-23 21:14:00 -0500

    Fix broken event card

--- app/lib/components/event_card.dart
+++ app/lib/components/event_card.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 class EventCard extends StatelessWidget {
-   EventCard({super.key, required this.event, this.onTap});
+  EventCard({super.key, required this.event, this.onTap});
 
   final RecordModel event;
   final void Function(RecordModel)? onTap;
@@ -22,7 +22,7 @@ class EventCard extends StatelessWidget {
     // Use this format: Sun Nov 14
     try {
       final date = DateTime.parse(dateTime);
-      return DateFormat('EEE MMM d').format(date);
+      return DateFormat('EEE MMM d - HH:mm').format(date);
     } catch (e) {
       return '';
     }
@@ -68,20 +68,43 @@ class EventCard extends StatelessWidget {
               ),
               const SizedBox(width: 12),
               Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      formatDateTime(event.data['start']),
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                    Text(
-                      event.expand['restaurant_id']?.first.data['restaurant_name'] ?? '',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.primaryContainer),
-                    ),
-                    const Spacer(),
-                    Text(event.data['title']),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatDateTime(event.data['start']),
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      Text(
+                        event.expand['restaurant_id']?.first.data['restaurant_name'] ?? '',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge!
+                            .copyWith(color: Theme.of(context).colorScheme.primaryContainer),
+                      ),
+                      const Spacer(),
+                      Text(event.data['title'], maxLines: 2, overflow: TextOverflow.ellipsis),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.music_note,
+                            size: 16,
+                          ),
+                          Text(event.data['genre']),
+                          const SizedBox(width: 12),
+                          const Icon(
+                            Icons.monetization_on_outlined,
+                            size: 16,
+                          ),
+                          const SizedBox(width: 2),
+                          Text(event.data['price'] == 0 ? 'Free' : '${(event.data['price'] / 100).toStringAsFixed(2)}'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
